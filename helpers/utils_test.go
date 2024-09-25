4138234f4f5439287bc0de2cf0f2451b8b2cbc0b
Eric Gotkowski <egotkowski@gmail.com>
2024-09-24 21:52:57 -0500

    adding unit tests for utilities

--- helpers/utils_test.go
+++ helpers/utils_test.go
@@ -3,16 +3,19 @@ package helpers_test
 import (
 	"strings"
 	"testing"
+	"unicode/utf8"
 
 	"github.com/egotch/gocowsay/helpers"
 )
 
-func Test_tabsToSpaces(t *testing.T) {
-
-  testLines := []string{
+var testLines = []string{
   "this\t is a test",
   "if\thandled correctly, should not have any \ttabs left",
-  }
+}
+
+
+func Test_tabsToSpaces(t *testing.T) {
+
 
   // positive test
   result := helpers.TabsToSpaces(testLines)
@@ -32,6 +35,35 @@ func Test_tabsToSpaces(t *testing.T) {
 
 }
 
-//TODO: add unit test for CalcMaxWidth
+func Test_calcMaxWidth(t *testing.T) {
+  
+  scrubbedText := helpers.TabsToSpaces(testLines)
 
-//TODO: add unit test for NormalizeLineLengh
+  // positive test
+  result := helpers.CalcMaxWidth(scrubbedText) 
+  if result != 58 {
+    t.Error("incorrect result: line length = ", result)
+  }
+
+  // negative test
+  if result == 0 {
+    t.Error("fail, result cannot be zero")
+  }
+}
+
+func Test_normalizeLineLength(t *testing.T)  {
+
+  scrubbedText := helpers.TabsToSpaces(testLines)
+  maxLen := helpers.CalcMaxWidth(scrubbedText)
+  result := helpers.NormalizeLineLen(scrubbedText, maxLen)
+
+
+  // postive test - test that all lines are the same length
+  currLen := utf8.RuneCountInString(result[0])
+  for _, l := range result {
+    if utf8.RuneCountInString(l) != currLen{
+      t.Error("incorrect result: line length doesnt match max line length", l)
+    }
+  }
+  
+}
